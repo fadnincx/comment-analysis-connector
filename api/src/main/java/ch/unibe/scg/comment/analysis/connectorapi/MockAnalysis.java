1918830b23c6ad5f4945b7c1d267755551dfe631
fadnincx <marcel.wuersten@students.unibe.ch>
2021-10-18 18:28:56 +0200

    finally working split up for arbitrary highlighting

--- api/src/main/java/ch/unibe/scg/comment/analysis/connectorapi/MockAnalysis.java
+++ api/src/main/java/ch/unibe/scg/comment/analysis/connectorapi/MockAnalysis.java
@@ -12,7 +12,9 @@ public class MockAnalysis {
         list.add(new AnalysisRange(100, 200, new String[]{"Summary", "Test"}));
         list.add(new AnalysisRange(220, 300, new String[]{"Summary"}));
         list.add(new AnalysisRange(400, 550, new String[]{"Exception"}));
-
+        list.add(new AnalysisRange(900, 1400, new String[]{"Code"}));
+        list.add(new AnalysisRange(2000, 2999, new String[]{"2k Code"}));
+        list.add(new AnalysisRange(4000, 4999, new String[]{"4k Code"}));
         return new AnalysisResult(0, "success", list);
     }
 }
--- chrome-plugin/content.js
+++ chrome-plugin/content.js
@@ -1,4 +1,9 @@
 /**
+ * Global variables
+ */
+let charCount2Line = [0];
+
+/**
  * Enable/Disable marking in the corner
  */
 const enableCornerMarking = function(){
@@ -29,7 +34,8 @@ const addTooltip = function(el, msg){
 	// Mark element
 	el.style.textDecorationLine = 'underline';
 	el.style.textDecorationColor = 'green';
-	el.style.textDecorationStyle = 'wavy';
+	// Seems buggy: https://bugs.chromium.org/p/chromium/issues/detail?id=668042
+	// el.style.textDecorationStyle = 'wavy';
 	
 	const tooltipId = Math.random().toString(36).substring(2,34);
 	el.addEventListener("mouseover", function( event ) {
@@ -55,6 +61,9 @@ const addTooltip = function(el, msg){
 	
 }
 
+/**
+ * Primitive check, if api url needs http prepended or / appended
+ */
 const getValidUrl = (url = "") => {
 
 	if ( url.indexOf("http") < 0){
@@ -67,21 +76,48 @@ const getValidUrl = (url = "") => {
 	return url;
 };
 
+/**
+ * Return the table element containing the source code
+ */
+const getCodeTable = function(){
+	return document.querySelectorAll("table.js-file-line-container")[0];
+}
+
+/**
+ * Wraps a text node into a span element node
+ */
+const wrapTextNode = function(txtNode){
+	if(txtNode.nodeType !== Node.TEXT_NODE){
+		return txtNode;
+	}
+	const wrapper = document.createElement('span')
+	wrapper.style.whiteSpace = 'pre'
+	txtNode.parentElement.replaceChild(wrapper,txtNode)
+	wrapper.appendChild(txtNode)
+	return wrapper
+}
+
+/**
+ * Returns the source code / filepath as string
+ */
 const getSourceFile = function (){
-	let el = document.querySelectorAll("table.js-file-line-container");
-	if (el.length < 1){
+	const table = getCodeTable();
+	if(table == null){
 		console.log("No code found!");
 		return '';
 	}
-	let table = el[0];
 	let file = "";
 	let totalChars = 0;
 	for(let i in table.rows){
+		charCount2Line[i] = totalChars;
 		let row = table.rows[i];
 		for(let j in row.cells){
 			let cell = row.cells[j];
 			for(k in cell.childNodes){
 				let child = cell.childNodes[k];
+				if(child.nodeType === Node.TEXT_NODE){
+					child = wrapTextNode(child)
+				}
 				if(child.textContent != null){
 					totalChars += child.textContent.length;
 					file += child.textContent;
@@ -93,17 +129,137 @@ const getSourceFile = function (){
 		}
 		file += "\n";
 	}
+	charCount2Line = charCount2Line.filter((x) =>  x != null)
 	return file;
 }
-
 const getFilePath = function (){
 	let el = document.querySelectorAll("h2#blob-path")
 	return el[0].innerText.replace(' / Jump to ','')
 }
 
+/**
+ * Splits a span element into a new element at given index
+ * @return the new element
+ */
+const splitElement = function (el, index){
+	if(el.tagName == null){
+		el = wrapTextNode(el)
+	}
+
+	// Create new DOM Element
+	const newElement = document.createElement('span')
+	// Copy classes from origin
+	newElement.classList = el.classList
+	newElement.innerHTML = el.innerHTML.substring(index)
+	el.innerHTML = el.innerHTML.substr(0, index)
+	el.after(newElement)
+	return newElement
+}
+
+/**
+ * Adding attribute with id to exactly the part that needs it
+ */
+const setAttributeForRange = function (id, from, to){
+
+	// Get the line in which the given range starts
+	let line = charCount2Line.filter((x) => x <= from).length - 1
+
+	// Initialize how many chars need to be passed until range begins
+	let startChar = from - charCount2Line[line]
+
+	// initialize how many chars are still needed for the range
+	let neededChars = to - from
+
+	console.log("At "+startChar+" (line "+line+") needes "+neededChars+" chars for "+id)
+
+	// if necessary do over multiple lines
+	for(;neededChars>0&&getCodeTable().rows.length>line;++line){
+
+		// Get the row and iterate over the cells
+		const row = getCodeTable().rows[line]
+		for(let i in row.cells){
+			const cell = row.cells[i]
+
+			console.log("Line "+line+" cell "+i);
+
+			// Iterate over all child elements of the cells
+			for(let j in cell.childNodes){
+				// If no further chars needed for the range, brake the loop
+				if (neededChars === 0) {
+					break;
+				}
+
+				let child = cell.childNodes[j]
+
+				// Get the text length of this child
+				let txtLength = 0;
+				if(child.textContent != null){
+					txtLength = child.textContent.length;
+					//console.log("'"+child.textContent+"' has length "+txtLength)
+				}else if (child.innerHTML != null){
+					txtLength = child.innerHTML.length;
+					console.warn("Do inner HTML!")
+				}
+				// if there is no text, continue with next child
+				if(txtLength <= 0){
+					continue;
+				}
+
+				// if range starts after this child, just reduce startChar by the text length
+				if(startChar > txtLength){
+					startChar -= txtLength;
+
+				// If range starts at the first character and range is longer than the text length, include whole child
+				}else if(startChar === 0 && neededChars >= txtLength){
+					child.setAttribute('commentAnalysisId',id)
+					neededChars -= txtLength;
+					startChar = 0
+
+				// if range starts at first character and ands within this child, split it up and include first part
+				}else if(startChar === 0){
+					splitElement(child, neededChars);
+					child.setAttribute('commentAnalysisId',id)
+					neededChars = 0
+
+				// if range doesn't start at first char but within this child, split it up and include second part
+				}else if (startChar > 0 && neededChars > txtLength-startChar){
+					const el = splitElement(child, startChar)
+					el.setAttribute('commentAnalysisId',id)
+					startChar = 0
+					neededChars -= (txtLength - startChar)
+
+				// Else means, the range starts and ends within this child. split it up twice and include only the middle part
+				}else{
+					console.log("Dangerous split twice")
+					const el = splitElement(child, startChar);
+					splitElement(el, neededChars);
+					el.setAttribute('commentAnalysisId',id)
+					neededChars = 0
+				}
+				console.log("Still needs "+neededChars)
+			}
+			// If no further chars needed for the range, brake the loop, no unnecessary loop through cells
+			if (neededChars === 0) {
+				break;
+			}
+		}
+	}
+}
+
+
 const applyResult = function (response){
 	console.log("Respones is")
 	console.log(response)
+	response.forEach(range => {
+		range.id =  Math.random().toString(36).substring(2,34);
+		setAttributeForRange(range.id, range.from, range.to)
+
+		document.querySelectorAll('[commentAnalysisId="'+range.id+'"]').forEach( el => {
+			addTooltip(el, range.labels.join(', '))
+		})
+
+	})
+
 }
 
 
@@ -121,6 +277,8 @@ var enable=function(){
 		return;
 	}
 	const srcFilePath = getFilePath();
+
+	console.log(charCount2Line)
 
 	// Read api address using the storage API
 	chrome.storage.sync.get('apiAddress', function(data) {
