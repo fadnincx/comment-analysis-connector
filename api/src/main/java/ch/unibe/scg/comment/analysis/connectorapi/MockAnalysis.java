acbd4e9171a88eb50524aa3b30f7e18b54bb13ad
fadnincx <marcel.wuersten@students.unibe.ch>
2021-10-27 13:46:31 +0200

    Adjust highlight hover background for better readability Add more labels to Mock

--- api/src/main/java/ch/unibe/scg/comment/analysis/connectorapi/MockAnalysis.java
+++ api/src/main/java/ch/unibe/scg/comment/analysis/connectorapi/MockAnalysis.java
@@ -9,12 +9,12 @@ public class MockAnalysis {
         Logger.getLogger("MockAnalysis").info("Mock Analysis for "+filepath);
         Logger.getLogger("MockAnalysis").info(src);
         ArrayList<AnalysisRange> list = new ArrayList<>();
-        list.add(new AnalysisRange(100, 200, new String[]{"Summary", "Test"}));
-        list.add(new AnalysisRange(220, 300, new String[]{"Summary"}));
-        list.add(new AnalysisRange(400, 550, new String[]{"Exception"}));
-        list.add(new AnalysisRange(900, 1400, new String[]{"Code"}));
-        list.add(new AnalysisRange(2000, 2999, new String[]{"2k Code"}));
-        list.add(new AnalysisRange(4000, 4999, new String[]{"4k Code"}));
+        list.add(new AnalysisRange(100, 200, new String[]{"Summary", "Test", "A super boring label"}));
+        list.add(new AnalysisRange(220, 300, new String[]{"Summary", "A super boring label"}));
+        list.add(new AnalysisRange(400, 550, new String[]{"Exception", "Test"}));
+        list.add(new AnalysisRange(900, 1400, new String[]{"Code", "A super boring label"}));
+        list.add(new AnalysisRange(2000, 2999, new String[]{"2k Code", "Code", "A super boring label"}));
+        list.add(new AnalysisRange(4000, 4999, new String[]{"4k Code", "Code"}));
         return new AnalysisResult(0, "success", list);
     }
 }
